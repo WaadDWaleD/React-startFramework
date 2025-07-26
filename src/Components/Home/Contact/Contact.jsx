060d3d43df53b0a653e1d7839e40ec4e5d298148
WaaDWaleD <waadwaled404@gmail.com>
2025-07-26 10:10:23 +0300

    New changes

--- src/Components/Home/Contact/Contact.jsx
+++ src/Components/Home/Contact/Contact.jsx
@@ -1,11 +1,77 @@
-<div className="input-container">
-  <input
-    id="uAge"
-    type="number"
-    className="form-control mb-5 border-0 border-bottom no-spinner"
-    placeholder="User Age"
-  />
-  <label htmlFor="uAge" className="input-label text-uppercase">
-    User Age
-  </label>
-</div>
+import React from "react";
+import "./Contact.css";
+
+export default function Contact() {
+  return (
+    <section
+      id="contact"
+      className="min-vh-100 d-flex justify-content-center align-items-center"
+    >
+      <div className="container">
+        <div className="title">
+          <h1 className="text-center display-6 fw-bold t-color">
+            CONTACT SECTION
+          </h1>
+
+          <div className="star-container mt-3 mb-3 d-flex justify-content-center align-items-center">
+            <div id="line"></div>
+            <i className="fa fa-solid fa-star t-color"></i>
+            <div id="line"></div>
+          </div>
+        </div>
+
+        <div className="w-50 mx-auto">
+          <div class="input-container">
+            <input
+              id="uName"
+              type="text"
+              className="form-control mt-5 mb-5 border-0 border-bottom"
+              placeholder="User Name"
+            />
+            <label for="uName" class="input-label text-uppercase ">
+              User Name
+            </label>
+          </div>
+
+          <div class="input-container">
+            <input
+              id="uAge"
+              type="number"
+              className="form-control mb-5 border-0 border-bottom"
+              placeholder="User Age"
+            />
+            <label for="uAge" class="input-label text-uppercase">
+              User Age
+            </label>
+          </div>
+
+          <div class="input-container">
+            <input
+              id="uEmail"
+              type="email"
+              className="form-control mb-5 border-0 border-bottom"
+              placeholder="User Email"
+            />
+            <label for="uEmail" class="input-label text-uppercase ">
+              User Email
+            </label>
+          </div>
+
+          <div class="input-container">
+            <input
+              id="uPass"
+              type="password"
+              className="form-control mb-4 border-0 border-bottom"
+              placeholder="User Password"
+            />
+            <label for="uPass" class="input-label text-uppercase">
+              User Password
+            </label>
+          </div>
+
+          <button className="btn btn-custom px-5">Submit</button>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,8 +9,6 @@ export default function Navbar() {
         <Link to="/home" className="text-decoration-none">
           <h1 className="my-0 ms-5 text-uppercase heading p-3">Start Framework</h1>
         </Link>
-
-        {/* Toggle button for small screens */}
         <button
           className="navbar-toggler text-white border-white"
           type="button"
@@ -22,8 +20,6 @@ export default function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-
-        {/* Navbar links */}
         <div className="collapse navbar-collapse justify-content-end" id="mainNavbar">
           <ul className="list-unstyled d-flex flex-lg-row flex-column align-items-lg-center me-5 my-auto mb-0">
             <li className="mx-3 my-2 my-lg-0">
